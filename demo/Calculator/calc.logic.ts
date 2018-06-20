f80dc7910b717857f9cc85ac1312a74abed35769
artur <artur@unao.pl>
2018-06-20 23:49:58 +0200

    fix/impr calculator

--- demo/Calculator/calc.logic.ts
+++ demo/Calculator/calc.logic.ts
@@ -19,24 +19,26 @@ const formNumber = (digit: Rx.Observable<Digit>, dot: Rx.Observable<any>, until:
     .merge(dot.take(1).mapTo('.'))
     .scan((acc, n) => n === '0' && (acc[0] === n) ? acc : acc.concat(n), [])
     .map(c => c.join(''))
-    .takeUntil(until)
+    .takeUntil(digit.take(1)
+      .switchMapTo(until))
     .defaultIfEmpty('0')
 
 export const logic: Logic = ({ uiEvents }) =>
-  Rx.Observable.concat(
+  $.concat(
     formNumber(uiEvents.digit, uiEvents.dot, uiEvents.operation)
       .map(n => ({ left: n })),
     formNumber(uiEvents.digit, uiEvents.dot, uiEvents.equal)
       .map(n => ({ right: n }))
   )
-    .merge(uiEvents.operation.take(1).map(o => ({ operation: o })))
+    .merge(uiEvents.digit.take(1).switchMapTo(uiEvents.operation)
+      .take(1).map(o => ({ operation: o })))
     .scan((acc, n) => Object.assign({}, acc, n), {} as UIStateIn)
     .switchMap(r => uiEvents.equal
-        .filter(() => 'right' in r)
-        .take(1)
-        .map(() => opHandlers[r.operation!](parseFloat(r.left!), parseFloat(r.right!)))
-        .map(r => ({ result: r.toString() }))
-        .startWith(r as UIState))
+      .filter(() => 'right' in r)
+      .take(1)
+      .map(() => opHandlers[r.operation!](parseFloat(r.left!), parseFloat(r.right!)))
+      .map(r => ({ result: r.toString() }))
+      .startWith(r as UIState))
     .do(x => console.log('STATE', x))
     .startWith({})
     .finally(() => console.log('DONE'))
