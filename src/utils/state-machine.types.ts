10239cfaab8e71a23b8be2d467c74e2d0a67646f
artur <artur@unao.pl>
2018-06-29 10:32:35 +0200

    machine progress

--- src/utils/state-machine.types.ts
+++ src/utils/state-machine.types.ts
@@ -1,15 +1,36 @@
-export type TransitionsProto<T extends {}> =
-  {} extends T ?
-  {} :
-  {
+import { Void } from './types'
+
+export type TransitionsProto<T> = {
     [K in keyof T]: T[K] extends {} ?
     (keyof T[K] extends keyof T ?
-      ({ [KK in keyof T[K]]: KK extends boolean ? T[K][KK] : never }) : never)
+      ({ [KK in keyof T[K]]: T[K][KK] extends boolean ? T[K][KK] : never }) : never)
     : never
   }
 
+export type Deeper<T extends {}> = {
+  [K in keyof T]?: T[K] extends MachineF$<any> ? T[K] : {}
+}
+
+class MachineF$<T extends TransitionsProto<T>, D extends Deeper<T> = {}> {
+  private _transitions: T
+  private _deeper: D
+
+  constructor (transitions: T, deeper = {} as D) {
+    this._transitions = transitions
+    this._deeper = deeper
+  }
+
+  deep = <S extends keyof T> (s: S) =>
+    this._deeper[s] as D[S] extends MachineF$<any> ? D[S] : undefined
+}
+
 const T = true
-const tr = { a: { a: T } }
+const tr = { a: { a: T, b: T }, b: { a: T, b: T } }
+const aTr = { c: { d: T }, d: { c: T } }
+
+const m = new MachineF$(tr, { a: new MachineF$(aTr) })
+
+const de = m.deep('a').deep('c')
 
 type Ks = keyof typeof tr
 type Tr = TransitionsProto<typeof tr>
