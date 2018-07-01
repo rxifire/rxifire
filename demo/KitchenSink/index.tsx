6307976504eb3777b5b178a6aeb70fb67b63482a
artur <artur@unao.pl>
2018-07-01 09:52:29 +0200

    fix component actions type to allow picking some subset of actions

--- demo/KitchenSink/index.tsx
+++ demo/KitchenSink/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Observable } from 'rxjs'
 
 import * as F$ from '../../src'
-import { mockActions, Actions, ActionsIO } from '../actions'
+import { mockActions, ActionsIO } from '../actions'
 
 const $ = Observable
 
@@ -23,7 +23,7 @@ export interface State {
   pos: Pos
 }
 
-type Spec = F$.ComponentSpec<State, Signals, ActionsIO,
+type Spec = F$.ComponentSpec<State, Signals, Pick<ActionsIO, 'randomNumbers'>,
   Behaviors, 'count' | 'open', { props: Observable<number> }>
 
 const actions = mockActions()
@@ -32,7 +32,7 @@ export const spec: Spec = {
   behaviorsDefaults: {
     name: '', dob: undefined as DoB | undefined, count: 0, x: '0', open: false
   },
-  actions: actions,
+  actions: actions as F$.ActionsF$<any>,
   animate: {
     count: (a) => a.map(n => n),
     open: () => Observable.empty()
@@ -42,14 +42,15 @@ export const spec: Spec = {
 const log: F$.Logic<Spec> = ({ beh, sig, act }) => {
   beh.$('dob')
   sig.$('click')
-  act.meta('error')
+  // act.meta('error')
   return Observable.of({ name: 'ab', pos: { x: 0, y: 0 } })
-    .merge($.timer(0, 200).map(x => ({ name: `${x}-`, pos: { x, y: -x } })))
-    .merge($.timer(2000).mergeMap(() => act.fire('randomNumbers')(20)).ignoreElements())
+    .merge($.timer(0, 1000).map(x => ({ name: `${x}-`, pos: { x, y: -x } })))
+    .merge($.timer(2000).mergeMap(() => act.fire('randomNumbers')(100)).ignoreElements())
     .delay(1)
 }
 
 const view: F$.JSXView<Spec> = ps => (s) => {
+ // ps.
   return <h1>HELLO ${s.name} {ps.meta.is('active') + ''} {ps.act.meta('randomNumbers').update + ps.act.meta('randomNumbers').status + ''}</h1>
 }
 
